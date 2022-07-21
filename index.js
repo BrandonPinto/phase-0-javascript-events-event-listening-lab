ea6e775161ba3698a6caed4732ed4ddc9604c81d
Brandon Pinto <b.pintony@gmail.com>
2022-07-21 09:13:07 -0400

    commit tree bb6fdaffa6a292ccab3bbbed5a9458ae2e0fbba7

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+//create variable for what you are selecting from
+//select button by using queryselector
+//create function to addingEventListener
+//addEventListener using the chosen variable
+//name event and use anon callback function (parameter)
+//return "string" 
+const input = document.querySelector('#button');
 function addingEventListener() {
+    input.addEventListener('click', function(e) {
+        return "clicked"
+    });
 }
